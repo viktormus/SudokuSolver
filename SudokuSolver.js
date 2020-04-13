d94baab63d98c29e20efb979c0213cf73da7b77d
viktormus <viktormus@gmail.com>
2020-04-12 19:59:20 -0500

    Improved version
    v2.0

--- SudokuSolver.js
+++ SudokuSolver.js
@@ -11,14 +11,19 @@ function intro() {
 }
 intro();
 
+//Declaring Some Variables
+var areTheySame = [];
+
+
 //Sum of all numbers in each nonet/row/column = 45
 
 //initial sudoku assemblement (starting pos)
 //each number slot is numbered: top left=first, bot right = last
 //0 means there is no number assigned, ie. it has to be solved
-var sudoku = [7, 0, 2, 6, 0, 9, 0, 0, 1, 0, 6, 0, 1, 5, 0, 0, 0, 7, 0, 1, 0, 0, 8, 2, 3, 0, 0, 0, 0, 0, 0, 3, 0, 5, 0, 8, 5, 0, 0, 8, 0, 7, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 9, 0, 0, 9, 0, 7, 0, 0, 0, 0, 0, 4, 6, 0, 1, 0, 0, 0, 0, 0, 0, 8, 5, 0, 0, 0, 0, 4];
-//Unable to solve the following sudoku:
-//var sudoku = [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 0, 0, 0, 0, 0, 0, 7, 0, 0, 9, 0, 2, 0, 0, 0, 5, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 4, 5, 7, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 6, 8, 0, 0, 8, 5, 0, 0, 0, 1, 0, 0, 9, 0, 0, 0, 0, 4, 0, 0];
+//var sudoku = [7, 0, 2, 6, 0, 9, 0, 0, 1, 0, 6, 0, 1, 5, 0, 0, 0, 7, 0, 1, 0, 0, 8, 2, 3, 0, 0, 0, 0, 0, 0, 3, 0, 5, 0, 8, 5, 0, 0, 8, 0, 7, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 9, 0, 0, 9, 0, 7, 0, 0, 0, 0, 0, 4, 6, 0, 1, 0, 0, 0, 0, 0, 0, 8, 5, 0, 0, 0, 0, 4];
+
+var sudoku = [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 0, 0, 0, 0, 0, 0, 7, 0, 0, 9, 0, 2, 0, 0, 0, 5, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 4, 5, 7, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 6, 8, 0, 0, 8, 5, 0, 0, 0, 1, 0, 0, 9, 0, 0, 0, 0, 4, 0, 0];
+
 
 //If sudoku.length = 81, it's correct
 //console.log(sudoku.length);
@@ -46,9 +51,9 @@ function printer() {
 printer();
 
 //Statistics
-var numbersToSolve = 0;
-var knownNumbers = 0;
 function stats() {
+  var numbersToSolve = 0;
+  var knownNumbers = 0;
   for (i = 0; i < sudoku.length; i++) {
     if (sudoku[i] == 0) {
       numbersToSolve++;
@@ -197,7 +202,7 @@ function leastUnknowns() {
     }
   }
 }
-leastUnknowns();
+//leastUnknowns();
 //console.log(leastN);
 //console.log(leastR);
 //console.log(leastC);
@@ -221,7 +226,7 @@ function venn() {
     }
   }
 }
-venn();
+//venn();
 //console.log(potentialEz);
 
 //Venn Diagram Generalization for all number slots
@@ -258,6 +263,7 @@ function convert(i, j) {
   }
 }
 
+
 function vennAll() {
   vennOptions = [];
   var tempList = [];
@@ -271,10 +277,6 @@ function vennAll() {
                 tempList.push(missingRows[i][l]);
               }
             }
-//           console.log(missingRows[i][l]);
-//           console.log(missingColumns[j][k]);
-//           console.log("...")
-
           }
         }
     }
@@ -284,7 +286,8 @@ function vennAll() {
   }
 }
 vennAll();
-//console.log(vennOptions);
+console.log("Possible Solutions [Venn Diagram]");
+console.log(vennOptions);
 
 
 //Substitution of [100% Certain] Solutions (individual numbers) and removing used solutions from Venn Diagram list - aka clearance
@@ -295,7 +298,7 @@ function convertToNonet(number) {
   return result;
 }
 function convertToRow(number) {
-  var result = Math.floor((number) / 9)
+  var result = Math.floor((number) / 9);
   return result;
 }
 function convertToColumn(number) {
@@ -303,54 +306,359 @@ function convertToColumn(number) {
   return result;
 }
 
-function substitute() {
+//There is a problem: if there are three potential spots for number 8 on the same row (at the same time, and 8 is an only option for that spot), the function will substitute all of them at the same time, which messes up the sudoku. This means that a previous number guess has been incorrect, as choosing eg. the first 8 will leave the other spots as 0, without further options available -> incomplete sudoku
+//Need to check for this in the substitute() function, and cancel it if such situation is encountered. On one hand, its PITA (pain in the ass), but on the other hand, there's no need for further guessing, and we can weed out the incorrect guesses and go back immediately
+
+function checkForMultiples() {
   for (i = 0; i < vennOptions.length; i++) {
-    
     if (vennOptions[i].length == 1) {
+      for (j = (i + 1); j < vennOptions.length; j++) {
+        if (vennOptions[j].length == 1 && vennOptions[i][0] == vennOptions[j][0]) {
+          console.log(i);
+          console.log(j);
+          console.log("Ok");
+          var oneNonet = convertToNonet(i);
+          var oneRow = convertToRow(i);
+          var oneColumn = convertToColumn(i);
+          var twoNonet = convertToNonet(j);
+          var twoRow = convertToRow(j);
+          var twoColumn = convertToColumn(j);
+          console.log(oneNonet);
+          console.log(twoNonet);
+          console.log(oneRow);
+          console.log(twoRow);
+          console.log(oneColumn);
+          console.log(twoColumn);
+          if (oneNonet == twoNonet || oneRow == twoRow || oneColumn == twoColumn) {
+            multipleNumber = vennOptions[i][0];
+            console.log("No Solution Available: " + multipleNumber);
+            return;
+          }
+        }
+      }
+    }
+  }
+}
+
+var multipleNumber = 0;
+var solvedNumbersOrder = [];
+var guessedNumbersPosition = [];
+function substitute() {
+  multipleNumber = 0;
+  checkForMultiples();
+//  console.log(multipleNumber);
+  if (multipleNumber != 0) {
+    console.log("THIS SHOULD RETURN");
+    printer();
+    return;
+  }
+  var areThereSolutions = [];
+  for (i = 0; i < vennOptions.length; i++) {  
+    if (vennOptions[i].length == 1) {
+      solvedNumbersOrder.push(i);
       var exit = vennOptions[i][0];
       sudoku.splice(i, 1, exit);
+      areThereSolutions.push(true);
     }
+  }
+  if (areThereSolutions[0] != true) {
+    checkTwos();
   }
   split();
   missing();
 }
-substitute();
+//substitute();
 console.log("--- --- ---");
+//check();
+
+
+//What to do when there are no 100% correct solutions available?
+//Just to let you know, future vigs, if you devine areTheySame immediately above this function, it doesn't work for some reason; the function executes prior to defining the variable...
+function checkTwos() {
+  for (i = 0; i < vennOptions.length; i++) {
+    if (vennOptions[i].length == 2) {
+      guessedNumbersPosition.push(i);
+      solvedNumbersOrder.push(i);
+      console.log(vennOptions[i]);
+//      console.log(vennOptions);
+      var secondNumber = vennOptions[i][1];
+      areTheySame.push(secondNumber);
+      var tryy = vennOptions[i][0];
+      sudoku.splice(i, 1, tryy);
+      break;
+    }
+  }
+}
+
+//Return substituted solutions to zero (clearance)
+function returnToZero(pos) {
+  for (j = pos; j < solvedNumbersOrder.length; j++) {
+    sudoku.splice(solvedNumbersOrder[j], 1, 0);
+  }
+  for (k = 0; k < backwards; k++) {
+    guessedNumbersPosition.pop();
+    areTheySame.pop();
+  }
+  console.log(guessedNumbersPosition);
+  console.log(areTheySame);
+  console.log(...solvedNumbersOrder);
+  var solidI = pos;
+//  console.log("solidI: " + solidI);
+  printer();
+  var del = 81 - solidI;
+  solvedNumbersOrder.splice(solidI, del);
+  split();
+  missing();
+  vennAll();
+}
+
+function goBack() {
+  console.log("goBack...");
+  console.log("currentGuess " + currentGuess);
+  console.log("vennOptions... " + vennOptions[guessedNumbersPosition[toBeReplaced]][1]);
+        if (currentGuess == vennOptions[guessedNumbersPosition[toBeReplaced]][1]) {
+        console.log("Both of the answer choices are exhausted and both of them are proved wrong. Taking another step back to try again...");
+        toBeReplaced--;
+        console.log("toBeReplaced: " + toBeReplaced);
+        for (i = 0; i < 81; i++) {
+          if (solvedNumbersOrder[i] == guessedNumbersPosition[toBeReplaced]) {
+            returnToZero(i);
+            guessedNumbersPosition.pop();
+            areTheySame.pop();
+            console.log("venn options for current pos: " + vennOptions[guessedNumbersPosition[toBeReplaced]]);
+            console.log("toBeReplaced: " + toBeReplaced);
+//            console.log(vennOptions);
+            console.log(guessedNumbersPosition);
+            console.log("second options: " + areTheySame);
+            break;
+          }
+        }
+        
+      }
+}
+
+//Try another number out of two
+var previousGuessedList = [];
+var toBeReplaced = 0;
+var currentGuess = 0;
+var backwards = 0;
+function tryAnother() {
+ //   var back = 1;
+ // if (sudoku[guessedNumbersPosition.length - 1] == ) {
+ //   back = 2;
+ //  guessedNumbersPosition.pop();
+ // }
+ // console.log(guessedNumbersPosition);
+  console.log(areTheySame);
+  
+  
+//  console.log("currentGuess: " + currentGuess);
+//  guessedNumbersPosition.pop();
+//  console.log("toBeReplaced: " + toBeReplaced);
+//  console.log("guessedNumbersPosition[toBeReplaced]: " + guessedNumbersPosition[toBeReplaced]);
+  backwards = 0;
+  for (i = (areTheySame.length - 1); i > -1; i--) {
+    if (sudoku[guessedNumbersPosition[i]] == areTheySame[i]) {
+      backwards++;
+      if (sudoku[guessedNumbersPosition[i - 1]] != areTheySame[i-1]) {
+        break;
+      }
+    }
+  }
+  console.log(guessedNumbersPosition);
+  console.log("Prev. G. List " + previousGuessedList);
+  var lastItem = guessedNumbersPosition.length - 1;
+  if (guessedNumbersPosition.length > previousGuessedList.length || (guessedNumbersPosition.length == previousGuessedList.length && sudoku[guessedNumbersPosition[lastItem]] != areTheySame[lastItem])) {
+    backwards = 0;
+  }
+  console.log("backwards: " + backwards);
+  toBeReplaced = guessedNumbersPosition.length - (1 + backwards);
+  currentGuess = sudoku[guessedNumbersPosition[toBeReplaced]];
+  console.log(toBeReplaced);
+  console.log(currentGuess);
+  console.log(guessedNumbersPosition);
+  console.log(guessedNumbersPosition[toBeReplaced]);
+  for (i = 0; i < 81; i++) {
+    if (solvedNumbersOrder[i] == guessedNumbersPosition[toBeReplaced]) {
+
+      returnToZero(i);
+      previousGuessedList = guessedNumbersPosition;
+      
+//      console.log("guessedNumbersPosition[toBeReplaced]: " + guessedNumbersPosition[toBeReplaced]);
+
+      /*
+      var backSteps = 0;
+      var dontMess = toBeReplaced;
+
+      for (k = (guessedNumbersPosition.length - 1); k > -1; k--) {
+        if (sudoku[guessedNumbersPosition[k]] == vennOptions[guessedNumbersPosition[k]][1]) {
+          backSteps++;
+          console.log("dontMess: " + dontMess);
+          console.log("vennOptions.guessedNumbersPosition[dontMess]: " + vennOptions[guessedNumbersPosition[dontMess]][1]);
+ //         console.log("currentGuess: " + currentGuess);
+          dontMess--;
+          
+        }
+      }
+      console.log("backSteps " + backSteps);
+      */
+
+
+
+/*
+      var deadLock = 0;
+      var go = true;
+      while (go) {
+        deadLock++;
+        goBack();
+        console.log("MOROMORO!");
+        console.log(sudoku[guessedNumbersPosition[toBeReplaced - 1]]);
+
+        var previousOne = sudoku[guessedNumbersPosition[toBeReplaced - 1]];
+        if (previousOne != areTheySame[guessedNumbersPosition.length - 2]) {
+          console.log("goBack loop stops here");
+          go = false;
+        } else {
+        console.log("goBack loop continues");
+        }
+        if (deadLock == 6) {
+          go = false;
+        }
+      }
+     */
+
+      var secondTry = vennOptions[guessedNumbersPosition[toBeReplaced]][1];
+      console.log("secondTry: " + secondTry);
+      sudoku.splice(guessedNumbersPosition[toBeReplaced], 1, secondTry);
+      console.log("...")
+      printer();
+      solvedNumbersOrder.push(guessedNumbersPosition[toBeReplaced]);
+//      console.log(guessedNumbersPosition);
+      console.log(...solvedNumbersOrder);
+      split();
+      missing();
+      vennAll();
+//      console.log(vennOptions);
+//      console.log("If I just print i: " + i);
+//      console.log("Printing solidI: " + solidI);
+//      console.log(vennOptions);
+//      console.log(guessedNumbersPosition[toBeReplaced]);
+      break;
+    }
+  }
+  
+//  printer();
+  stats();
+}
+
+//Recalls tryAnother() when necessary
+function tryAnotherLoop() {
+
+}
+
+//One Solver Loop for Testing
+function check() {
+  vennAll();
+  console.log(vennOptions);
+}
 
 
 //Solver Loop
 var run = true;
-var holder = 0;
 var safetyStop = 0;
 function solverLoop() {
   console.log("Initiating Solver Algorithm...");
   while (run) {
+    var zerosStart = 0;
+    var zerosEnd = 0;
+
+    for (i = 0; i < sudoku.length; i++) {
+      if (sudoku[i] == 0) {
+        zerosStart++;
+        
+      } 
+    }
+    console.log("zerosStart: " + zerosStart);
+
     safetyStop++;
     vennAll();
     substitute();
 //    printer();
 //    console.log("...");
-    var zeroCheck = 0;
-    holder = 0;
+
+    if (multipleNumber != 0) {
+      console.log("doubleNumber -> no solution. Running tryAnother()");
+      tryAnother();
+    }
+
     for (i = 0; i < sudoku.length; i++) {
       if (sudoku[i] == 0) {
-        holder++;
+        zerosEnd++;
       } 
     }
-    if (holder == 0) {
-     run = false;
+    console.log("zerosEnd: " + zerosEnd);
+
+//Tähän koodi että jos on saman verran nollia kaks kierrosta putkeen, eli ratkasu ei etene, nii menee tuohon tryAnother funktioon
+    if (zerosStart == zerosEnd) {
+      if (zerosStart == 0) {
+        printer();
+        console.log("Congratulations! Your sudoku has been solved.");
+        console.log("Time taken: ???");
+        run = false;
+      } else {
+      printer();
+//      console.log(...solvedNumbersOrder);
+      console.log("The looper did " + safetyStop + " iterations before realizing that there is no solution with current numbers options.");
+      tryAnother();
+ //     safetyStop = 0;
+ //    run = false;
+      }
     }
-    if (safetyStop == 50) {
+
+    if (safetyStop == 10000) {
+      console.log("safetyStop limit (10000 iterations) reached!");
+      printer();
+      console.log("Unable to solve... damn shame.");
       run = false;
     }
-  }
-  printer();
-  if (safetyStop == 50) {
-    console.log("Unable to solve... damn shame.");
-  } else {
-    console.log("Congratulations! Your sudoku has been solved.");
-    console.log("Time taken: ???");
   }
 }
 
 solverLoop();
+console.log("Misc Stats:");
+console.log("'Challenged' by Eemeli Kyröläinen");
+console.log("Time taken to code this: 17-21h [1-3h + 6h + 3-5h + 7h]");
+
+/*
+console.log(guessedNumbersPosition);
+console.log("...");
+console.log(...solvedNumbersOrder);
+printer();
+function testRun(rounds) {
+  for (a = 0; a < rounds; a++) {
+    vennAll();
+    console.log("Round " + (a + 1));
+//    console.log(vennOptions);
+    substitute();
+    printer();
+  }
+}
+*/
+
+/*
+testRun(22);
+console.log(guessedNumbersPosition);
+console.log(...solvedNumbersOrder);
+tryAnother();
+testRun(5);
+console.log(guessedNumbersPosition);
+console.log("...");
+console.log(...solvedNumbersOrder);
+tryAnother();
+testRun(8);
+tryAnother();
+testRun(5);
+tryAnother();
+testRun(14);
+tryAnother();
+*/
